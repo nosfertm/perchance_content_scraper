afa00245562daf6c2409904175eb98c5b9d99abb
nosfertm <nosfertm@gmail.com>
2025-02-28 16:43:56 -0300

    upd files for second run

--- ai-character-chat/characters/sfw/Carrio by birbbrained/src/customCode.js
+++
@@ -1,8 +0,0 @@
-oc.thread.on("MessageAdded", async function() {
-  oc.thread.messages.forEach(a => {
-    a.avatar = {
-      size: oc.character.avatar.size,
-      shape: oc.character.avatar.shape
-    };
-  });
-});
